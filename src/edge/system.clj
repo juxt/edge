e34e2c71b1183aea20da6bdb0fe6fa17da3ab5f5
Malcolm Sparks <malcolm@juxt.pro>
2016-01-13 21:28:25 +0000

    Simplified back to base

--- src/edge/system.clj
+++ src/edge/system.clj
@@ -4,27 +4,13 @@
   (:require
    [clojure.java.io :as io]
    [clojure.string :as str]
-   [com.stuartsierra.component :refer (system-map system-using using)]
-   [edge.db :refer [new-dburi-generator new-database new-seeder]]
-   [edge.trq :refer [new-transaction-report-queue]]
-   [edge.log-stream :refer [new-log-stream]]))
+   [com.stuartsierra.component :refer (system-map system-using using)]))
 
 (defn new-system-map [opts]
-  (system-map
-   ::dburi (new-dburi-generator)
-   ::database (new-database)
-   ::seeder (new-seeder)
-   ::trq (new-transaction-report-queue)
-   ::log (new-log-stream)))
+  (system-map))
 
 (defn new-dependency-map []
-  {::trq {:database ::database
-          :dburi ::dburi}
-   ::database {:dburi ::dburi}
-   ::seeder {:database ::database
-             :dburi ::dburi}
-   ::log {:trq ::trq
-          :dburi ::dburi}})
+  {})
 
 (defn new-production-system
   "Create the production system"
