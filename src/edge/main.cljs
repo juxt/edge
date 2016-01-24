7b1ad823b36ac2d3e848f53c2273bfa25291a8c8
Malcolm Sparks <malcolm@juxt.pro>
2016-01-24 19:24:03 +0000

    wip

--- src/edge/main.cljs
+++ src/edge/main.cljs
@@ -18,17 +18,21 @@
           (dom/div nil
             (dom/h3 nil (get-in (om/props this) [:title]))
             (dom/ul nil
-              (map garment (get-in (om/props this) [:garments])))
-          )))
+              (map garment (get-in (om/props this) [:garments]))))))
 
 (def garment-list (om/factory GarmentList))
 
+(def app-state {:title "Dominic"
+                :garments [{:garments-by-sku 1}
+                           ]
+                :garments-by-sku {1 {:description "Dress"}
+                                  2 {:description "Shoes"}
+                                  3 {:description "Bra"}
+                                  4 {:description "Knickers"}
+                                  5 {:description "Trousers"}}})
+
 (defn init []
   (enable-console-print!)
-  (js/ReactDOM.render (garment-list {:title "Dominic"
-                                     :garments [{:description "Dress"}
-                                                {:description "Shoes"}
-                                                {:description "Bra"}
-                                                {:description "Knickers"}]})
+  (js/ReactDOM.render (garment-list (om/db->tree app-state))
                     (gdom/getElement "app"))
   (println "Hello world!"))
