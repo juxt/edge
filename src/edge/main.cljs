32713cac18d4482d0841a103734f4ad62ae4e453
Malcolm Sparks <malcolm@juxt.pro>
2016-01-24 21:50:35 +0000

    counter

--- src/edge/main.cljs
+++ src/edge/main.cljs
@@ -5,25 +5,33 @@
    [om.dom :as dom])
   )
 
-(defui Garment
+#_(defui Garment
   Object
   (render [this]
           (dom/li nil (get (om/props this) :description))))
 
-(def garment (om/factory Garment))
+#_(def garment (om/factory Garment))
 
-(defui GarmentList
+#_(defui GarmentList
+  static om/IQuery
+  (query [this]
+    [:title :garments])
+  
   Object
   (render [this]
-          (dom/div nil
-            (dom/h3 nil (get-in (om/props this) [:title]))
-            (dom/ul nil
-              (map garment (get-in (om/props this) [:garments]))))))
+          (let [{:keys [title garments]} (om/props this)]
+            (dom/div nil
+              (dom/h3 nil title)
+              (dom/ul nil
+                (for [g garments]
+                  (dom/li nil (:description g))))             
+              ))))
 
-(def garment-list (om/factory GarmentList))
+#_(def garment-list (om/factory GarmentList))
 
-(def app-state {:title "Dominic"
-                :garments [{:garments-by-sku 1}
+#_(def app-state {:title "Dominic"
+                :garments [[:garments-by-sku 1]
+                           [:garments-by-sku 2]
                            ]
                 :garments-by-sku {1 {:description "Dress"}
                                   2 {:description "Shoes"}
@@ -31,8 +39,38 @@
                                   4 {:description "Knickers"}
                                   5 {:description "Trousers"}}})
 
+(def app-state (atom {:count 0}))
+
+(defn read [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
+      {:value :not-found})))
+
+(defn mutate [{:keys [state] :as env} key params]
+  (if (= 'increment key)
+    {:value {:keys [:count]}
+     :action #(swap! state update-in [:count] inc)}
+    {:value :not-found}))
+
+(defui Counter
+  static om/IQuery
+  (query [this]
+         [:count])
+  Object
+  (render [this]
+          (let [{:keys [count]} (om/props this)]
+            (dom/div nil
+        (dom/span nil (str "Count: " count))
+        (dom/button
+          #js {:onClick
+               (fn [e] (om/transact! this '[(increment)]))}
+          "Click me!")))))
+
+(def reconciler
+  (om/reconciler {:state app-state
+                  :parser (om/parser {:read read :mutate mutate})}))
+
 (defn init []
   (enable-console-print!)
-  (js/ReactDOM.render (garment-list (om/db->tree app-state))
-                    (gdom/getElement "app"))
-  (println "Hello world!"))
+  (om/add-root! reconciler Counter (gdom/getElement "app")))
