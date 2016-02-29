409945e667e2b50af2edb30cd4a43842bf92f947
Malcolm Sparks <malcolm@juxt.pro>
2016-02-29 08:13:42 +0000

    Dependency updates

--- project.clj
+++ project.clj
@@ -4,15 +4,14 @@
   :description "Project template"
   :url "http://github.com/juxt/edge"
 
-  :exclusions [com.stuartsierra/component]
+  :pedantic? :abort
 
   :dependencies
   [
    ;; Infrastructure
    [com.stuartsierra/component "0.3.1"]
-   [prismatic/schema "1.0.4"]
+   [prismatic/schema "1.0.5"]
    [org.clojure/core.async "0.2.374"]
-   [org.clojure/tools.reader "0.10.0"]
 
    ;; Logging
    [org.clojure/tools.logging "0.3.1"]
@@ -22,8 +21,9 @@
    [ch.qos.logback/logback-classic "1.1.3" :exclusions [org.slf4j/slf4j-api]]
 
    ;; Web
-   [aleph "0.4.1-beta3"]
-   [bidi "1.24.0"]
+   [aleph "0.4.1-beta5"]
+   [bidi "2.0.0" :exclusions [commons-codec]]
+
    [hiccup "1.0.5"]
    [org.omcljs/om "1.0.0-alpha28"]
    [yada "1.1.0-20160126.014942-13"]
@@ -35,6 +35,6 @@
                  :welcome (println "Type (dev) to start")}
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [org.clojure/tools.namespace "0.2.10"]
+                                  [org.clojure/tools.namespace "0.2.11"]
                                   [reloaded.repl "0.2.1"]]
                    :source-paths ["dev"]}})
