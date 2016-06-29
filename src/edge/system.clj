a5dcf29883f5a1f9829c469390e6319591c3879e
Malcolm Sparks <malcolm@juxt.pro>
2016-06-29 18:50:09 +0100

    Code comments added

--- src/edge/system.clj
+++ src/edge/system.clj
@@ -7,27 +7,39 @@
    [clojure.java.io :as io]
    [com.stuartsierra.component :refer [system-map system-using]]
    [edge.selmer :refer [new-selmer]]
-   [edge.server :refer [new-web-server]]
+   [edge.web-server :refer [new-web-server]]
    [edge.phonebook.db :as db]))
 
-(defn config [profile]
+(defn config
+  "Read EDN config, with the given profile. See Aero docs at
+  https://github.com/juxt/aero for details."
+  [profile]
   (aero/read-config (io/resource "config.edn") {:profile profile}))
 
-(defn configure-system [system config]
+(defn configure-system
+  "Merge configuration to its corresponding component (prior to the
+  system starting). This is a pattern described in
+  https://juxt.pro/blog/posts/aero.html"
+  [system config]
   (merge-with merge system config))
 
-(defn new-system-map []
+(defn new-system-map
+  "Create the system. See https://github.com/stuartsierra/component"
+  []
   (system-map
    :web-server (new-web-server)
    :selmer (new-selmer)
    :db (db/create-db {100 {:surname "Sparks" :firstname "Malcolm" :phone "000256"}
                       101 {:surname "Pither" :firstname "Jon" :phone "342339"}})))
 
-(defn new-dependency-map []
+(defn new-dependency-map
+  "Declare the dependency relationships between components. See
+  https://github.com/stuartsierra/component"
+  []
   {})
 
 (defn new-system
-  "Construct a new system with the given profile"
+  "Construct a new system, configured with the given profile"
   [profile]
   (-> (new-system-map profile)
       (configure-system (config profile))
