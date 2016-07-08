e770e77069803c3953469546f51a5fab80601bd6
Lombide Carreton Andoni <alombide@prod.telenet.be>
2016-07-08 15:03:12 +0200

    Changed remaining phonebook db operations to work with MongoDB.

--- src/edge/phonebook/db.clj
+++ src/edge/phonebook/db.clj
@@ -6,7 +6,8 @@
    [schema.core :as s]
    [edge.phonebook.schema :refer [Phonebook PhonebookEntry]]
    [monger.core :as mg]
-   [monger.collection :as mc])
+   [monger.collection :as mc]
+   [monger.operators :refer :all])
   (:import [org.bson.types ObjectId]))
 
 (def phonebook-colname "phonebook")
@@ -19,6 +20,11 @@
 (s/defn create-db [dbconfig
                    entries :- Phonebook]
   (let [db (get-mongo-db (:host dbconfig) (:dbname dbconfig))]
+  ; Add text index for free text search on phonebook entries
+  (mc/ensure-index db phonebook-colname {
+     :surname "text"
+     :firstname "text"
+   })
   db))
 
 
@@ -56,27 +62,39 @@
    id))
 
 (s/defn get-entries :- Phonebook
+  "Get all entries"
   [db]
   (mc/find-maps db phonebook-colname))
 
-;  TODO
-(s/defn matches? [q :- String
-                 entry :- PhonebookEntry]
-  (some (partial re-seq (re-pattern (str "(?i:\\Q" q "\\E)")))
-        (map str (vals (second entry)))))
 
-; TODO
+; Fields of a phonebook entry to be searched using text search.
+(def phonebook-entry-fields-to-be-searched
+  (vector :surname :firstname))
+
+(defn make-regex [field query]
+  { field { $regex (str ".*"  query ".*") $options "i" }})
+
+(defn make-regex-search-vector [query]
+  (mapv
+    (fn [field] (make-regex field query))
+    phonebook-entry-fields-to-be-searched))
+
+; We text search phonebook fields by doing a MongoDB regexp on individual fields in combination
+; with the MongoDB $or operator.
+; A better solution would be to use MongoDB's text index and perform a real free
+; text search. Unfortunately, this version of Monger does not implement this MongoDB operator.
 (s/defn search-entries :- Phonebook
+  "Free text search on some entry fields"
   [db q]
-  (let [entries (get-entries db)
-        f (filter (partial matches? q) entries)]
-    (into {} f)))
+  (mc/find-maps db phonebook-colname { $or (make-regex-search-vector q)}))
 
 (s/defn get-entry :- (s/maybe PhonebookEntry)
+  "Get entry with id"
   [db id]
   (mc/find-map-by-id db phonebook-colname id))
 
 
 (s/defn count-entries :- s/Int
+  "Count the entries"
   [db]
   (mc/count db phonebook-colname))
