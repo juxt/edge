4df7243ca069a4aac89fee2ae1b71e85a1003303
Lombide Carreton Andoni <alombide@prod.telenet.be>
2016-07-08 11:37:12 +0200

    Adapted inserting and finding to MongoDB.

--- src/edge/phonebook/db.clj
+++ src/edge/phonebook/db.clj
@@ -5,8 +5,11 @@
    [clojure.tools.logging :refer :all]
    [schema.core :as s]
    [edge.phonebook.schema :refer [Phonebook PhonebookEntry]]
-   [monger.core :as mg]))
+   [monger.core :as mg]
+   [monger.collection :as mc])
+  (:import [org.bson.types ObjectId]))
 
+(def phonebook-colname "phonebook")
 
 (s/defn get-mongo-db [host db]
    (let [conn (mg/connect host)
@@ -16,24 +19,24 @@
 (s/defn create-db [dbconfig
                    entries :- Phonebook]
   (let [db (get-mongo-db (:host dbconfig) (:dbname dbconfig))]
-    (assert entries)
-    {:phonebook (ref entries)
-     :next-entry (ref (if (not-empty entries)
-                        (inc (apply max (keys entries)))
-                        1))}))
+  db))
+
+
+; We rely on ObjectId to generate the next db id, but we do not store it as such.
+; Instead, we store it as its hash code. The reason is that yada needs an int as an id
+; to generate a URI for the newly created resource. For now, we prefer this solution
+; than to change to yada implementation to be able to handle ObjectIds.
+(defn get-next-db-id []
+  (.hashCode (ObjectId.)))
 
 (defn add-entry
   "Add a new entry to the database. Returns the id of the newly added
   entry."
   [db entry]
   (dosync
-   ;; Why use 2 refs when one atom would do? It comes down to being able
-   ;; to return nextval from this function. While this is possible to do
-   ;; with an atom, its feels less elegant.
-   (let [nextval @(:next-entry db)]
-     (alter (:phonebook db) conj [nextval entry])
-     (alter (:next-entry db) inc)
-     nextval)))
+    (let [record-id (get-next-db-id)]
+      (mc/insert db phonebook-colname (assoc entry :_id record-id))
+      record-id)))
 
 (defn update-entry
   "Update a new entry to the database. Returns the id of the newly added
@@ -50,7 +53,7 @@
 
 (s/defn get-entries :- Phonebook
   [db]
-  @(:phonebook db))
+  (mc/find-maps db phonebook-colname))
 
 (s/defn matches? [q :- String
                  entry :- PhonebookEntry]
@@ -65,7 +68,7 @@
 
 (s/defn get-entry :- (s/maybe PhonebookEntry)
   [db id]
-  (get @(:phonebook db) id))
+  (mc/find-map-by-id db phonebook-colname id))
 
 (s/defn count-entries :- s/Int
   [db]
