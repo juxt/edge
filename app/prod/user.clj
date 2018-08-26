29fadac7dfaa1ff03aea0c402321dc1dfd45f476
Malcolm Sparks <malcolm@juxt.pro>
2018-08-27 00:49:52 +0100

    Add production build

---
+++ app/prod/user.clj
@@ -0,0 +1,8 @@
+(ns user
+  (:require
+   [edge.main :refer [system]]))
+
+;; Used in production REPL. The intention is to provide, optionally, a
+;; REPL in production.
+
+;; Helper functions below:
--- app/src/edge/main.clj
+++ app/src/edge/main.clj
@@ -5,12 +5,52 @@
   "Main entrypoint for production instances"
   (:gen-class)
   (:require
+   [clojure.tools.namespace.repl :as repl]
    [integrant.core :as ig]
    [edge.system :refer [system-config]]))
 ;; end::ns[]
 
+(repl/disable-reload!)
+
+(def system {})
+
+(defonce system-agent (agent {}))
+
+(defn resume [profile]
+  (println "Resuming system")
+  (ig/init (system-config profile)))
+
 (defn -main [& args]
-  (let [system-config (system-config :prod)] ; <1>
-    (ig/init system-config)) ; <2>
-  ;; All threads are daemon, so block forever:
-  @(promise))
+
+  (set-error-handler!
+    system-agent
+    (fn [a error]
+      (println "ERROR:" error)
+      ;; TODO: Restart the agent
+      ))
+
+  (add-watch
+    system-agent
+    :reset
+    (fn [k r old new]
+      (alter-var-root #'system (constantly new))))
+
+  (let [profile (or (some-> (first args) keyword) :prod)
+        system-config (system-config profile)]
+    (send system-agent (constantly (ig/init system-config)))
+
+    (let [bindings (get-thread-bindings)]
+      (sun.misc.Signal/handle
+        (sun.misc.Signal. "HUP")
+        (reify sun.misc.SignalHandler
+          (handle [_ signal]
+            (send
+              system-agent
+              (fn [system]
+                (with-bindings bindings
+                  (println "Signal HUP received, halting!")
+                  (ig/halt! system)
+                  (println "System halted. Refreshing code")
+                  (repl/refresh)
+                  (println "Code refreshed. Resuming system.")
+                  (resume profile))))))))))
