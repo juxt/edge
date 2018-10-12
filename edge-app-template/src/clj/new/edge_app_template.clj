cea3287838bd9ce1807f7ab50005c2a72a2b0459
Dominic Monroe <dominic@juxt.pro>
2018-10-12 07:18:54 +0100

    Add easy way to create new apps

---
+++ edge-app-template/src/clj/new/edge_app_template.clj
@@ -0,0 +1,16 @@
+(ns clj.new.edge-app-template
+  (:require [clj.new.templates :refer [renderer project-name name-to-path ->files]]))
+
+(def render (renderer "app.template"))
+
+(defn edge-app-template
+  "FIXME: write documentation"
+  [name]
+  (let [data {:name (project-name name)
+              :sanitized (name-to-path name)}]
+    (println "Generating fresh 'clj new' edge.app-template project.")
+    (->files data
+             ["deps.edn" (render "deps.edn" data)]
+             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)]
+             ["src/config.edn" (render "config.edn" data)]
+             ["dev/dev.clj" (render "dev.clj" data)])))
