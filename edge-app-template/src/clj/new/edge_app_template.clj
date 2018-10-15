a86fcb536366fabe2e2e2039299b8aab48507579
Dominic Monroe <dominic@juxt.pro>
2018-10-15 12:10:25 +0100

    Split --kick flag into --sass and --cljs
    This allows users to be more selective about what their application will
    do.

--- edge-app-template/src/clj/new/edge_app_template.clj
+++ edge-app-template/src/clj/new/edge_app_template.clj
@@ -12,18 +12,28 @@
         data {:name (project-name name)
               :sanitized (name-to-path name)
               :root-ns (multi-segment (sanitize-ns name))
-              :kick (contains? opts :kick)}]
+              :sass (contains? opts :sass)
+              :cljs (contains? opts :cljs)
+              :kick (or (contains? opts :sass)
+                        (contains? opts :cljs))}]
     (println "Generating fresh 'clj new' edge.app-template project.")
     (->files data
              ["deps.edn" (render "deps.edn" data)]
              ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)]
              ["src/config.edn" (render "config.edn" data)]
              ["dev/dev.clj" (render "dev.clj" data)])
-    (when (:kick data)
-      (binding [*force?* true]
+    (binding [*force?* true]
+      (when (:kick data)
         (->files data
-                 ["src/{{sanitized}}/frontend/main.cljs" (render "main.cljs" data)]
-                 ["src/{{name}}.scss" (render "app.scss" data)]
                  ["src/index.html" (render "index.html" data)]
                  ["target/dev/.gitkeep" ""]
-                 ["target/prod/.gitkeep" ""])))))
+                 ["target/prod/.gitkeep" ""]))
+      (if (:sass data)
+        (->files data
+                 ["src/{{name}}.scss" (render "app.scss" data)])
+        (->files data
+                 ["src/public/{{name}}.css" (render "app.css" data)]))
+      (when (:cljs data)
+        (->files data
+                 ["src/{{sanitized}}/frontend/main.cljs" (render "main.cljs" data)]))
+      )))
