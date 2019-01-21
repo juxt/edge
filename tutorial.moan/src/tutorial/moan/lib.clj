13581e230407d3653a4b1ec4c1a5f05773842f3b
Dominic Monroe <dominic@juxt.pro>
2019-01-21 07:41:51 +0000

    Mark mock tweets as favorites

--- tutorial.moan/src/tutorial/moan/lib.clj
+++ tutorial.moan/src/tutorial/moan/lib.clj
@@ -7,8 +7,10 @@
   [{:id 1
     :text "This is a fantastic tweet"
     :author {:name "Elon Musk"
-             :username "fakeelonmusk"}}
+             :username "fakeelonmusk"}
+    :favorite? true}
    {:id 2
     :text "Lord but I dislike poetry. How can anyone remember words that aren't put to music?"
     :author {:name "Arliden"
-             :username "arl_the_bard"}}])
+             :username "arl_the_bard"}
+    :favorite? true}])
