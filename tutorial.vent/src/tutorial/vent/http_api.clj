0fa0b4c0d658e2c4e0d7380a82abedb7b49c219e
Dominic Monroe <dominic@juxt.pro>
2019-01-30 15:51:58 +0000

    Migrate away from deprecated authentication API

--- tutorial.vent/src/tutorial/vent/http_api.clj
+++ tutorial.vent/src/tutorial/vent/http_api.clj
@@ -2,12 +2,9 @@
   (:require
     [integrant.core :as ig]
     [yada.yada :as yada]
+    yada.handler
     [yada.security :refer [verify]]
     [clojure.walk :as walk]))
-
-(defmethod verify ::constant
-  [ctx _]
-  "developer")
 
 (defn- resolve-params
   [m pull]
@@ -19,38 +16,39 @@
 
 (defn- api-resource
   [res]
-  (yada/resource
-    (-> res
-        (assoc :access-control
-               {:realms {"default"
-                         {:authentication-schemes
-                          [{:scheme ::constant}]}}})
-        (update :methods
-                (fn [methods]
-                  (reduce-kv
-                    (fn [methods k v]
-                      (let [sym (::symbol v)]
-                        (require (symbol (namespace sym)))
+  (yada.handler/insert-interceptor
+    (yada/handler
+      (yada/resource
+        (-> res
+            (update :methods
+                    (fn [methods]
+                      (reduce-kv
+                        (fn [methods k v]
+                          (let [sym (::symbol v)]
+                            (require (symbol (namespace sym)))
 
-                        (-> (walk/postwalk
-                              (fn [x]
-                                (if (= 'String x)
-                                  String
-                                  x))
-                              methods)
+                            (-> (walk/postwalk
+                                  (fn [x]
+                                    (if (= 'String x)
+                                      String
+                                      x))
+                                  methods)
 
-                            (update-in [k :produces]
-                                       (fnil conj #{})
-                                       "application/edn")
-                            (assoc-in [k :response]
-                                      (fn [ctx]
-                                        (def ctx ctx)
-                                        (if-let [params (::params v)]
-                                          ((resolve sym)
-                                           (resolve-params ctx params))
-                                          ((resolve sym))))))))
-                    methods
-                    methods))))))
+                                (update-in [k :produces]
+                                           (fnil conj #{})
+                                           "application/edn")
+                                (assoc-in [k :response]
+                                          (fn [ctx]
+                                            (def ctx ctx)
+                                            (if-let [params (::params v)]
+                                              ((resolve sym)
+                                               (resolve-params ctx params))
+                                              ((resolve sym))))))))
+                        methods
+                        methods))))))
+    yada.security/authorize
+    (fn [ctx]
+      (assoc ctx :authentication "developer"))))
 
 (defmethod ig/init-key ::api-resource
   [_ res]
