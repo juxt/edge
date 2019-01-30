d9f292065e376cfef4ac4e328fc4db722d498c74
Dominic Monroe <dominic@juxt.pro>
2019-01-30 15:23:16 +0000

    Add hot reloading example to edge app template

--- edge-app-template/resources/clj/new/app.template/main.cljs
+++ edge-app-template/resources/clj/new/app.template/main.cljs
@@ -1,5 +1,11 @@
-(ns {{root-ns}}.frontend.main)
+(ns ^:figwheel-hooks {{root-ns}}.frontend.main)
 
 (js/console.log "Hello, world")
 
-(set! (.-innerText (js/document.getElementById "app")) "Loaded {{name}}!")
+;; This is called once
+(defonce init
+  (do (set! (.-innerText (js/document.getElementById "app")) "Loaded {{name}}!") true))
+
+;; This is called every time you make a code change
+(defn ^:after-load reload []
+  (set! (.-innerText (js/document.getElementById "app")) "Reloaded {{name}}!"))
