f8eb0355a3c6a5343917683c5b6046df42bf7c21
Dominic Monroe <dmc@juxt.pro>
2019-02-25 12:23:55 +0000

    Add edge-cljs-repl to generated .dir-locals.el
    Fix #62

--- edge-app-template/resources/clj/new/app.template/dir-locals.el
+++ edge-app-template/resources/clj/new/app.template/dir-locals.el
@@ -1,2 +1,4 @@
 ((nil
-  (cider-clojure-cli-global-options . "-A:dev{{#kick}}:build:dev/build{{/kick}}")))
+  (cider-clojure-cli-global-options . "-A:dev{{#kick}}:build:dev/build{{/kick}}"){{#cljs}}
+  (cider-default-cljs-repl . edge)
+  (cider-cljs-repl-types . ((edge "(cljs-repl)"))){{/cljs}}))
