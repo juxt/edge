a28f0b993cf1f9d0e345fec0717cc18aaa8e6972
Dominic Monroe <dmc@juxt.pro>
2019-02-25 14:09:17 +0000

    Add refresh functions to default dir-locals

--- edge-app-template/resources/clj/new/app.template/dir-locals.el
+++ edge-app-template/resources/clj/new/app.template/dir-locals.el
@@ -1,4 +1,6 @@
 ((nil
+  (cider-refresh-before-fn . "dev-extras/suspend")
+  (cider-refresh-after-fn  . "dev-extras/resume")
   (cider-clojure-cli-global-options . "-A:dev{{#kick}}:build:dev/build{{/kick}}"){{#cljs}}
   (cider-default-cljs-repl . edge)
   (cider-cljs-repl-types . ((edge "(cljs-repl)"))){{/cljs}}))
