68fd8892f667069b892afcf42f9ea5d3a6ad253b
Dominic Monroe <dmc@juxt.pro>
2019-02-26 06:55:45 +0000

    Add auto-dev for emacs

--- edge-app-template/resources/clj/new/app.template/dir-locals.el
+++ edge-app-template/resources/clj/new/app.template/dir-locals.el
@@ -1,6 +1,7 @@
 ((nil
   (cider-refresh-before-fn . "dev-extras/suspend")
   (cider-refresh-after-fn  . "dev-extras/resume")
+  (cider-repl-init-code . ("(dev)"))
   (cider-clojure-cli-global-options . "-A:dev{{#kick}}:build:dev/build{{/kick}}"){{#cljs}}
   (cider-default-cljs-repl . edge)
-  (cider-cljs-repl-types . ((edge "(cljs-repl)"))){{/cljs}}))
+  (cider-cljs-repl-types . ((edge "(user/cljs-repl)"))){{/cljs}}))
