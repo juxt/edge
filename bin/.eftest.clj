668f127c29720501fe346c079cd66203aaaef4be
Dominic Monroe <dmc@juxt.pro>
2019-03-14 20:45:11 +0000

    Correctly set exit code from eftest

--- bin/.eftest.clj
+++ bin/.eftest.clj
@@ -1,11 +1,9 @@
 (require '[eftest.runner :refer [find-tests run-tests]])
 (require 'orchard.query)
 
-(run-tests
-  (find-tests
-    ;; This is slow, but because requiring namespaces is slow
-    (orchard.query/namespaces {:load-project-ns? true
-                               :project? true})))
-
-;; Some agent, somewhere
-(shutdown-agents)
+(let [summary (run-tests
+                (find-tests
+                  ;; This is slow, but because requiring namespaces is slow
+                  (orchard.query/namespaces {:load-project-ns? true
+                                             :project? true})))]
+  (System/exit (+ (:error summary) (:fail summary))))
