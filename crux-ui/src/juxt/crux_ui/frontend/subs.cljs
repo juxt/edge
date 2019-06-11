6fa481729dbed91bd29d8b630e1f39832d9a7944
Ivan Fedorov <hello@spacegangster.io>
2019-06-11 10:58:36 +0300

    console: teach query analysis to recognise txes

--- crux-ui/src/juxt/crux_ui/frontend/subs.cljs
+++ crux-ui/src/juxt/crux_ui/frontend/subs.cljs
@@ -1,6 +1,6 @@
 (ns juxt.crux-ui.frontend.subs
   (:require [re-frame.core :as rf]
-            [medley.core :as m]
+            [juxt.crux-ui.frontend.logic.query-analysis :as qa]
             [cljs.reader]))
 
 (rf/reg-sub :subs.query/input-committed  (fnil :db.query/input-committed  false))
@@ -28,35 +28,6 @@
             :<- [:subs.query/input-edn]
             #(:error % false))
 
-(defn calc-vector-headers [query-vector]
-  (->> query-vector
-       (drop-while #(not= :find %))
-       (rest)
-       (take-while #(not= (keyword? %)))))
-
-(defn analyze-full-results-headers [query-results-seq]
-  (let [res-count (count query-results-seq)
-        sample (if (> res-count 50)
-                 (random-sample (/ 50 res-count) query-results-seq)
-                 query-results-seq)]
-    (set (flatten (map (comp keys :crux.query/doc) sample)))))
-
-(defn query-vec->map [qv]
-  (let [raw-map
-          (->> qv
-               (partition-by keyword?)
-               (partition 2)
-               (into {}))]
-    (-> raw-map
-        (update :find vec)
-        (m/update-existing :full-results? first)
-        (update :where vec))))
-
-(defn calc-query-info [input-edn]
-  (if (vector? input-edn)
-    (query-vec->map input-edn)
-    input-edn))
-
 (rf/reg-sub
   :subs.query/info
   :<- [:subs.query/input-edn-committed]
@@ -64,7 +35,7 @@
     (cond
       (not input-edn)      nil
       (:error input-edn)   nil
-      :else (calc-query-info input-edn))))
+      :else (qa/calc-query-info input-edn))))
 
 (rf/reg-sub
   :subs.query/headers
@@ -73,7 +44,7 @@
   (fn [[q-res q-info]]
     (when q-info
       (if (:full-results? q-info)
-         (analyze-full-results-headers q-res)
+         (qa/analyze-full-results-headers q-res)
          (:find q-info)))))
 
 
