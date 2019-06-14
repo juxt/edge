eea380b5842b7630d826d6d4d19fa43c03f628b9
Ivan Fedorov <hello@spacegangster.io>
2019-06-14 13:34:10 +0300

    drop edge.kick from integrant config; add shadow-prod.sh to build production cljs

--- crux-ui/run.sh
+++ crux-ui/run.sh
@@ -1,4 +1,5 @@
 # ../bin/rebel --nrepl -A:dev:dev/build:build
 # export PATH_TO_FX=/home/jdt/lib/javafx-sdk-11.0.2/lib
 # export PATH_TO_FX=/home/jdt/lib/javafx-sdk-11.0.2/lib
-../bin/rebel --nrepl -A:dev:dev/build:build #-R:rebl
+
+../bin/rebel --nrepl -A:dev
--- crux-ui/shadow-dev.sh
+++ crux-ui/shadow-dev.sh
@@ -1,1 +1,2 @@
+#! /usr/bin/env sh
 shadow-cljs watch dev
