7e7a56d887c863e5d482a316f75dbd89a7616f69
Malcolm Sparks <mal@juxt.pro>
2020-01-21 09:54:51 +0000

    Use loader in core

--- juxt.calcite-play/src/juxt/calcite_play/core.clj
+++ juxt.calcite-play/src/juxt/calcite_play/core.clj
@@ -1,6 +1,8 @@
 (ns juxt.calcite-play.core
   (:require
    [crux.api :as crux]
+   [clojure.java.io :as io]
+   [juxt.calcite-play.sw-load :as sw-load]
    juxt.calcite-play.schema-factory)
   (:import (crux.api ICruxAPI)
            (org.apache.calcite.jdbc CalciteConnection)))
@@ -8,23 +10,24 @@
 (def ^crux.api.ICruxAPI node
   (crux/start-node {:crux.node/topology :crux.standalone/topology
 	            :crux.node/kv-store "crux.kv.memdb/kv"
-	            :crux.kv/db-dir "data/db-dir-1"
-	            :crux.standalone/event-log-dir "data/eventlog-1"
+	            :crux.kv/db-dir "data/db-dir-3"
+	            :crux.standalone/event-log-dir "data/eventlog-3"
 	            :crux.standalone/event-log-kv-store "crux.kv.memdb/kv"}))
 
 (crux/submit-tx
  node
- [[:crux.tx/put
-   {:crux.db/id :dbpedia.resource/Pablo-Picasso ; id
-    :name "Pablo"
-    :last-name "Picasso"}
-   ]])
+ (for [doc (sw-load/res->crux-docs (io/resource "swapi/resources/fixtures/planets.json"))]
+   [:crux.tx/put doc]))
 
-(crux/q
- (crux/db node)
- '{:find [e]
-   :where [[e :name "Pablo"]]})
+;
+(vec (crux/q
+      (crux/db node)
+      '{:find [e climate]
+        :where [[e :crux.db/id id]
+                [e :climate climate]]}))
 
-(let [conn (java.sql.DriverManager/getConnection "jdbc:calcite:model=src/juxt/calcite_play/model.json")
+#_(java.sql.DriverManager/getConnection "jdbc:crux:db1")
+
+#_(let [conn (java.sql.DriverManager/getConnection "jdbc:calcite:model=src/juxt/calcite_play/model.json")
       stmt (.createStatement conn)]
   (resultset-seq (.executeQuery stmt "select sku, description from product limit 2")))
