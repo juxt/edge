c5d39c30e37e017f440f4d7e651e91f8b837a4fd
Malcolm Sparks <mal@juxt.pro>
2020-01-21 10:18:02 +0000

    Complete set of SW resources

--- juxt.calcite-play/src/juxt/calcite_play/sw_load.clj
+++ juxt.calcite-play/src/juxt/calcite_play/sw_load.clj
@@ -7,21 +7,43 @@
   [fields]
   (reduce-kv (fn [acc k v]  (conj acc [(keyword k) v])) {} fields))
 
+;; All the resource paths making up Star Wars DB
+(def resource-paths
+  ["swapi/resources/fixtures/people.json"
+   "swapi/resources/fixtures/planets.json"
+   "swapi/resources/fixtures/films.json"
+   "swapi/resources/fixtures/species.json"
+   "swapi/resources/fixtures/vehicles.json"
+   "swapi/resources/fixtures/starships.json"])
+
+;; Primary keys are strings of the form resources.<name> which we need to map to
+;; paths in the canonical URI
 (def model->path
-  {"resources.planet" "planets"})
+  {"resources.film" "films"
+   "resources.people" "people"
+   "resources.planet" "planets"
+   "resources.species" "species"
+   "resources.starship" "starships"
+   "resources.vehicle" "vehicles"})
 
 (defn ->crux-doc [json]
-    (let [{:strs [fields model pk]} json
-          id (java.net.URI. (format "https://swapi.co/api/%s/%s/" (model->path model) pk))]
-      (-> fields
-          keywordize-fields
-          (conj {:crux.db/id id}))))
+  (let [{:strs [fields model pk]} json
+        path (model->path model)
+        _ (assert path (format "nil path with model %s" model))
+        id (java.net.URI. (format "https://swapi.co/api/%s/%s/" path pk))]
+    (-> fields
+        keywordize-fields
+        (conj {:crux.db/id id}))))
 
 (defn res->crux-docs [res]
   (map ->crux-doc (json/parse-stream (io/reader res))))
 
 (comment
-  (first (json/parse-stream (io/reader (io/resource "swapi/resources/fixtures/planets.json")))))
+  (take 4 (json/parse-stream (io/reader (io/resource "swapi/resources/fixtures/vehicles.json")))))
+
+(comment
+  (for [res resource-paths]
+    (res->crux-docs (io/resource res))))
 
 (comment
   (res->crux-docs (io/resource "swapi/resources/fixtures/planets.json")))
