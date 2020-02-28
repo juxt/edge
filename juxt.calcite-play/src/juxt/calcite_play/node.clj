19a5793ac4ce546f9438f3000dec88091168235d
Zyxmn Daley Jes <zyx@juxt.pro>
2020-02-28 11:01:57 +0000

    crux example query comments

--- juxt.calcite-play/src/juxt/calcite_play/node.clj
+++ juxt.calcite-play/src/juxt/calcite_play/node.clj
@@ -16,3 +16,20 @@
   node
   (for [doc (sw-load/crux-docs)]
     [:crux.tx/put doc]))
+
+
+(comment
+
+(crux/submit-tx
+ node
+ [[:crux.tx/put
+   {:crux.db/id :dbpedia.resource/Pablo-Picasso ; id
+    :name "Pablo"
+    :last-name "Picasso"}
+   #inst "2018-05-18T09:20:27.966-00:00"]]) ; valid time
+
+(crux/q (crux/db node)
+        '{:find [e]
+          :where [[e :name "Pablo"]]})
+
+)
