ec40d9cc92bcb7e815955b4ed3919831bdbd78f1
Malcolm Sparks <mal@juxt.pro>
2020-08-25 23:21:21 +0100

    Add content-negotiation

--- examples/juxt.mmxx/src/juxt/mmxx/handler.clj
+++ examples/juxt.mmxx/src/juxt/mmxx/handler.clj
@@ -4,17 +4,55 @@
   (:require
    [crux.api :as crux]
    [juxt.spin.alpha.handler :as spin.handler]
-   [juxt.spin.alpha.resource :as spin.resource]))
+   [juxt.spin.alpha.resource :as spin.resource]
+   [juxt.spin.alpha.server :as spin.server]
+   [juxt.pick.alpha.core :refer [pick]]
+   [juxt.pick.alpha.apache :refer [using-apache-algo]]
+   [juxt.reap.alpha.ring :refer [decode-accept-headers]]
+   [juxt.reap.alpha.decoders :refer [content-type]]))
+
+(def memoized-content-type
+  (memoize
+   (fn [arg]
+     (content-type arg))))
 
 (defn create-handler [opts]
   (let [crux (:crux opts)]
     (spin.handler/handler
      (reify
+
        spin.resource/ResourceLocator
        (locate-resource [_ uri]
          (let [e (crux/entity (crux/db crux) (java.net.URI. (.getPath uri)))]
            e))
+
        spin.resource/Resource
        (invoke-method [resource-provider server-provider resource response request respond raise]
-         (respond {:status 200 :body (:content resource)})))
-     (reify))))
+         (let [method (:request-method request)]
+           (case method
+             (:head :get)
+             (respond
+              (cond-> response
+                (= method :get) (conj {:body (:content resource)})))
+
+             :put
+             (respond (assoc response :body "TODO\n")))))
+
+       spin.resource/ContentNegotiation
+       (best-representation [resource-provider resource request]
+         (let [db (crux/db crux)]
+           (pick
+            using-apache-algo
+            (conj {}
+                  (decode-accept-headers request)
+                  [:juxt.http/variants
+                   (map #(-> (crux/entity db %)
+                             (update :juxt.http/content-type memoized-content-type))
+                        (:juxt.http/variants resource))
+                   ])))))
+
+     (reify
+       spin.server/ServerOptions
+       (server-header [_] "JUXT MMXX Example Server")
+       (server-options [_] nil)
+       ))))
--- examples/juxt.mmxx/src/juxt/mmxx/seeder.clj
+++ examples/juxt.mmxx/src/juxt/mmxx/seeder.clj
@@ -3,11 +3,40 @@
 (ns juxt.mmxx.seeder
   (:require
    [integrant.core :as ig]
-   [crux.api :as crux]))
+   [crux.api :as crux]
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
 (defmethod ig/init-key ::seeder [_ {:keys [crux]}]
   (crux/submit-tx
    crux
-   [[:crux.tx/put
-     {:crux.db/id (java.net.URI. "/")
-      :content "Hello World!"}]]))
+   (for [resource
+         [
+          {:crux.db/id (java.net.URI. "/documents/Crux-Form")
+           :juxt.http/variants
+           [(java.net.URI. "/documents/Crux-Form.adoc")
+            (java.net.URI. "/documents/Crux-Form.html")]
+           :juxt.http/methods #{:get :options}}
+
+          {:crux.db/id (java.net.URI. "/documents/Crux-Form.adoc")
+           ;; slightly privilege the raw adoc over the html
+           :juxt.http/quality-of-source 0.9
+           :juxt.http/content-type "text/plain;charset=utf-8"
+           :juxt.http/methods #{:get :put :options}
+           :content (str/join "\n" (take 20 (line-seq (io/reader (io/file (System/getProperty "user.home") "src/team/juxt.crux/collateral/projects/form-whitepaper/Crux-Form.adoc")))))}
+
+          {:crux.db/id (java.net.URI. "/documents/Crux-Form.html")
+           :juxt.http/quality-of-source 0.8
+           :juxt.http/content-type "text/html;charset=utf-8"
+           :juxt.http/methods #{:get :options}
+           :content "<h2>TODO: This will be the HTML 'type' of the content</h2>\n"}]]
+
+     [:crux.tx/put
+      (cond-> resource
+        ;; Set the juxt.http/uri to the same as the id
+        true (assoc :juxt.http/uri (:crux.db/id resource))
+        ;; Set the content length
+        (:content resource) (assoc :juxt.http/content-length (count (:content resource)))
+        )]
+
+     )))
