9c16724de7282337d7508c66c360885c359394ff
Malcolm Sparks <mal@juxt.pro>
2020-09-02 23:09:04 +0100

    Revert from https to http
    Selmer URL paths cannot work with https with current self-signed openssl cert.

--- examples/juxt.mmxx/src/juxt/mmxx/seeder.clj
+++ examples/juxt.mmxx/src/juxt/mmxx/seeder.clj
@@ -16,24 +16,24 @@
   (crux/submit-tx
    crux
    (for [resource
-         [;; Can this be PUT by using a special content-type of application/vnc.crux.entity+edn ?
+         [ ;; Can this be PUT by using a special content-type of application/vnc.crux.entity+edn ?
           ;; Or via the http-server?
           ;; See https://www.iana.org/assignments/media-types/media-types.xhtml
           ;; Note, we can also add Content-Location as a request header, which solves this problem.
 
           {:crux.db/id :spin/root
-           :juxt.http/uri (new URI "https://localhost:2020/")
+           :juxt.http/uri (new URI "http://localhost:2020/")
            :juxt.http/redirect :spin/readme}
 
           ;; A resource which does proactive content negotiation to find the best representation
           {:crux.db/id :spin/readme
-           :juxt.http/uri (new URI "https://localhost:2020/spin/README")
+           :juxt.http/uri (new URI "http://localhost:2020/spin/README")
            :juxt.http/variants
            [:spin/readme-adoc :spin/readme-html]
            :juxt.http/methods #{:get :options}}
 
           {:crux.db/id :spin/readme-adoc
-           :juxt.http/uri (new URI "https://localhost:2020/spin/README.adoc")
+           :juxt.http/uri (new URI "http://localhost:2020/spin/README.adoc")
 
            ;; A resource corresponding to the adoc representation of the README
            :juxt.http/base64-encoded-payload
@@ -53,7 +53,7 @@
 
           ;; A resource corresponding to the adoc representation of the README
           {:crux.db/id :spin/readme-html
-           :juxt.http/uri (new URI "https://localhost:2020/spin/README.html")
+           :juxt.http/uri (new URI "http://localhost:2020/spin/README.html")
 
            :juxt.http/base64-encoded-payload
            (.encodeToString
